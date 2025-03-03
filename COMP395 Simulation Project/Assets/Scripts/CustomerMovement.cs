7583d7e49bcafc08ae311bda60b88e5d1fc6a2dc
Mashtixx <nquanghuy3456@gmail.com>
2025-03-02 21:57:59 -0500

    Fix CustomerMovement code

--- COMP395 Simulation Project/Assets/Scripts/CustomerMovement.cs
+++ COMP395 Simulation Project/Assets/Scripts/CustomerMovement.cs
@@ -2,33 +2,35 @@ using UnityEngine;
 
 public class CustomerMovement : MonoBehaviour
 {
-    public GameObject startPoint; // Assign in inspector
     public GameObject targetPoint; // Assign in inspector
     public float moveDuration = 3f; // Time it takes to reach target
 
     private float timer; // Internal timer
     private bool isMoving = false;
+    Vector3 startPos;
+
 
     void Start()
     {
         // Optional: Automatically start moving on play
         StartMovement();
+        
     }
 
     public void StartMovement()
     {
+        startPos = transform.position;
         timer = 0f;
-        transform.position = startPoint.transform.position;
         isMoving = true;
     }
 
     void Update()
     {
-        if (isMoving && startPoint != null && targetPoint != null)
+        if (isMoving && targetPoint != null)
         {
             timer += Time.deltaTime;
             float t = Mathf.Clamp01(timer / moveDuration); // Normalize time (0 to 1)
-            transform.position = Vector3.Lerp(startPoint.transform.position, targetPoint.transform.position, t);
+            transform.position = Vector3.Lerp(startPos, targetPoint.transform.position, t);
 
             if (t >= 1f)
             {
